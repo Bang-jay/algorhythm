f89673f9b82230bebc06002982fcc1cf32f28b97
방재희 <60768614+Bang-jay@users.noreply.github.com>
2021-04-11 17:46:36 +0900

    Update baekjoon11399.cpp

--- baekjoon11399.cpp
+++ baekjoon11399.cpp
@@ -3,26 +3,26 @@
 int main(){
 	int n, min, i, result, k, output;
 	result = 0;
-	output = 0:
+	output = 0;
 	scanf("%d",&n);
 	int pArray[n];
 	for(i=0;i<n;i++){
 		scanf("%d", &pArray[i]);
 	}
-	for(i=0;i<n;i++){
+	for(int c=0;c<n;c++){
 		min = pArray[0];
 		k = 0;
 		for(i=1;i<n;i++){
 			if(min > pArray[i]){
 			k = i;
 			min = pArray[i];
-			break;
 			}
 		}
 		pArray[k] = 9999;
-		result += min;
-		output += result;
+		output += result += min;
 	}
+
+	printf("%d", output);
 	
 	return 0;
 }
